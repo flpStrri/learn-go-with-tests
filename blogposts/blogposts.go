8728ce14d8b6d4e726b7b3080be5a7220db9fb10
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2024-10-15 08:15:05 -0300

    finish go-fundamentals/reading-files

---
+++ blogposts/blogposts.go
@@ -0,0 +1,73 @@
+package blogposts
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/fs"
+	"strings"
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
+}
+
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+func newPost(postBody io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postBody)
+
+	return Post{
+		Title:       readMetaLine(scanner, titleSeparator),
+		Description: readMetaLine(scanner, descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(scanner, tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readMetaLine(scanner *bufio.Scanner, tagName string) string {
+	scanner.Scan()
+	return strings.TrimPrefix(scanner.Text(), tagName)
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
+}
---
+++ blogposts/go.mod
@@ -0,0 +1,3 @@
+module github.com/flpStrri/blogposts
+
+go 1.22.5
